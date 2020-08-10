c63cabddf2de20c60ea66cb9cf5d459ed96d39a4
dacardenasa <diego.cardenasaleg@gmail.com>
2020-08-10 14:20:36 -0500

    Remove body parser from app

--- app.js
+++ app.js
@@ -1,50 +1,52 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 
 // Set pug template config
-app.set('view engine', 'pug');
-app.set('views', './public/views');
+app.set("view engine", "pug");
+app.set("views", "./public/views");
 
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mongo-1', 
-{ useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/mongo-1", {
+  useNewUrlParser: true,
+});
 
-mongoose.connection.on('error', (error) => console.error(error.message));
+mongoose.connection.on("error", (error) => console.error(error.message));
 
-let schema = mongoose.Schema({
-  name: String,
-  email: String,
-  password: String
-}, { collection: 'users' });
+let schema = mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    password: String,
+  },
+  { collection: "users" }
+);
 
-const User = mongoose.model('User', schema);
+const User = mongoose.model("User", schema);
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   const users = await User.find();
-  res.render('index', { users: users });
+  res.render("index", { users: users });
 });
 
-app.get('/register', (req, res) => {
-  res.render('form');
+app.get("/register", (req, res) => {
+  res.render("form");
 });
 
-app.post('/register', async (req, res) => {
-  const user = new User({ 
-    name: req.body.name, 
+app.post("/register", async (req, res) => {
+  const user = new User({
+    name: req.body.name,
     email: req.body.email,
-    password: req.body.password 
+    password: req.body.password,
   });
 
   await user.save((error) => {
     if (error) return console.error(error.message);
-    return res.redirect('/');
+    return res.redirect("/");
   });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
-
